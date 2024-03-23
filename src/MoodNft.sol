26aef39ac821ddf66d966638fe643163752d16e5
Komron Khisomov <normok_9595@bk.ru>
2024-03-24 02:54:53 +0500

    testViewTokenURI

---
+++ src/MoodNft.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract MoodNft is ERC721 {
+    uint256 private s_tokeCounter;
+    string private s_sadSvgImageUri;
+    string private s_happySvgImageUri;
+
+    enum Mood{
+        HAPPY,
+        SAD
+    }
+    mapping(uint256 => Mood) private s_tokenIdToMood;
+
+    constructor(
+        string memory sadSvgImageUri,
+        string memory happySvgImageUri
+    ) ERC721("Mood NFT", "MN") {
+        s_tokeCounter = 0;
+        s_sadSvgImageUri = sadSvgImageUri;
+        s_happySvgImageUri = happySvgImageUri;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokeCounter);
+        s_tokenIdToMood[s_tokeCounter] = Mood.HAPPY;
+        s_tokeCounter++;
+    }
+
+    function _baseURI() internal pure override returns(string memory){
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        string memory imageURI;
+        if(s_tokenIdToMood[tokenId] == Mood.HAPPY){
+            imageURI = s_happySvgImageUri;
+        }else{
+            imageURI = s_sadSvgImageUri;
+        }
+
+        return string(abi.encodePacked(_baseURI(), Base64.encode(
+            bytes(abi.encodePacked(
+                            '{"name":"',
+                            name(), // You can add whatever name here
+                            '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
+                            '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
+                            imageURI,
+                            '"}'
+                        )
+                )
+        ))
+        
+        );
+        
+    }
+}
