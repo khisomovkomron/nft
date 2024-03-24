d40de7d3e4646e50bbd60f47a5e30bcc5c1013c7
Komron Khisomov <normok_9595@bk.ru>
2024-03-24 14:43:53 +0500

    integrations test

--- test/unit/DeployMoodNftTest.t.sol
+++ test/unit/DeployMoodNftTest.t.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
-import {DeployMoodNft} from "../script/DeployMoodNft.s.sol";
-import {MoodNft} from "../src/MoodNft.sol";
+import {DeployMoodNft} from "../../script/DeployMoodNft.s.sol";
+import {MoodNft} from "../../src/MoodNft.sol";
 
 contract DeployMoodNftTest is Test {
     DeployMoodNft public deployer;
@@ -15,7 +15,7 @@ contract DeployMoodNftTest is Test {
     function testConvertSvgToUri() public view {
         string
             memory expectedUri = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwIiBoZWlnaHQ9IjEwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICA8Y2lyY2xlIGN4PSI1MCIgY3k9IjUwIiByPSI0MCIgc3Ryb2tlPSJncmVlbiIgc3Ryb2tlLXdpZHRoPSI0IiBmaWxsPSJ5ZWxsb3ciIC8+Cjwvc3ZnPg==";
-        strting
+        string
             memory svg = '<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg"> <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" /></svg>';
         string memory actualUri = deployer.svgToImageURI(svg);
         assert(keccak256(abi.encodePacked(actualUri)) == keccak256(abi.encodePacked(expectedUri)));
