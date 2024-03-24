1b6a6ede11bb05f0ece88a6a879b60c8630f2c8a
Komron Khisomov <normok_9595@bk.ru>
2024-03-24 15:21:09 +0500

    DeployMoodNftTest

--- Makefile
+++ Makefile
@@ -45,11 +45,11 @@ deploy:
 mint:
 	@forge script script/Interactions.s.sol:MintBasicNft ${NETWORK_ARGS}
 
-# deployMood:
-# 	@forge script script/DeployMoodNft.s.sol:DeployMoodNft $(NETWORK_ARGS)
+deployMood:
+	@forge script script/DeployMoodNft.s.sol:DeployMoodNft $(NETWORK_ARGS)
 
-# mintMoodNft:
-# 	@forge script script/Interactions.s.sol:MintMoodNft $(NETWORK_ARGS)
+mintMoodNft:
+	@forge script script/Interactions.s.sol:MintMoodNft $(NETWORK_ARGS)
 
 # flipMoodNft:
 # 	@forge script script/Interactions.s.sol:FlipMoodNft $(NETWORK_ARGS)
--- test/unit/DeployMoodNftTest.t.sol
+++ test/unit/DeployMoodNftTest.t.sol
@@ -18,6 +18,8 @@ contract DeployMoodNftTest is Test {
         string
             memory svg = '<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg"> <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" /></svg>';
         string memory actualUri = deployer.svgToImageURI(svg);
-        assert(keccak256(abi.encodePacked(actualUri)) == keccak256(abi.encodePacked(expectedUri)));
+        console.log(expectedUri);
+        console.log(actualUri);
+        assertEq(keccak256(abi.encodePacked(actualUri)),keccak256(abi.encodePacked(expectedUri)));
     }
 }
